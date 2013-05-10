4aaaaa7226ca48c4334cfb806c0096c0bf010244
Kirill Kalinin <kalininkc@hotmail.com>
2013-05-10 14:18:31 +0600

    implementation of sigmoid function has been added.

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -8,9 +8,9 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
+sizes = size(z)
 
-
-
+g = ones(sizes) ./ (ones(sizes) + repmat(e, sizes) .^ -z)
 
 
 % =============================================================
